559faa461cdd9404a887b8a758bc28b73c2ee676
Alan Panayotov <alanpanayotov@gmail.com>
2020-09-06 01:07:53 +0100

    basic styling of room login page

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
 const port = process.env.PORT || 80
+
+app.use('/', express.static(path.join(__dirname, "..", "public")))
 
 app.set('view engine', 'pug')
 app.get('/', (req, res) => {
