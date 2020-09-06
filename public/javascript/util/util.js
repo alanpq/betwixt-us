1b22b842907a79156e65ab6a575172ec3836f01c
Alan Panayotov <alanpanayotov@gmail.com>
2020-09-06 04:08:58 +0100

    misc jsdocs + colour array

--- public/javascript/util/util.js
+++ public/javascript/util/util.js
@@ -13,7 +13,31 @@ function debounce(func, wait, immediate = false) {
   };
 };
 
+/**
+ * Fill and Stroke the same text at once.
+ * @param {CanvasRenderingContext2D} ctx 
+ * @param {string} text 
+ * @param {number} x 
+ * @param {number} y 
+ * @param {number} maxWidth 
+ */
 const fillStrokedText = (ctx, text, x, y, maxWidth) => {
   ctx.fillText(text, x, y, maxWidth);
   ctx.strokeText(text, x, y, maxWidth);
 }
+
+// TODO: real colours
+const colors = [
+  "#ffffff",
+  "#ff0faa",
+  "#fccfff",
+  "#faaabf",
+  "#ffddff",
+  "#003dff",
+  "#fbfff",
+  "#deadbe",
+  "#beefda",
+  "#123faa",
+  "#addaaa",
+  "#cccaac",
+];
