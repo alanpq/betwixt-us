8c7bc777a17e0eeac1260dc4f970deb33a6e5010
cakyman <alanpanayotov@gmail.com>
2020-09-15 18:09:02 +0100

    imagine using 4 spaces

--- public/javascript/interactables.js
+++ public/javascript/interactables.js
@@ -20,7 +20,7 @@ export const interactables = [];
 
 let highlightShader;
 hookPreload(async () => {
-    highlightShader = twgl.createProgramInfo(gl, [await loadShader("vertex"), await loadShader("f_highlight")])
+  highlightShader = twgl.createProgramInfo(gl, [await loadShader("vertex"), await loadShader("f_highlight")])
 });
 
 /**
@@ -29,21 +29,21 @@ hookPreload(async () => {
  * @param {any} cb 
  */
 export const addInteractable = async (interactable, cb) => {
-    interactable._sprite = await sprite.getSprite(gl, interactable.sprite)
-    interactable.id = interactables.length;
-    interactable.interactCB = cb;
-    interactables.push(interactable);
-    return interactable.id;
+  interactable._sprite = await sprite.getSprite(gl, interactable.sprite)
+  interactable.id = interactables.length;
+  interactable.interactCB = cb;
+  interactables.push(interactable);
+  return interactable.id;
 }
 
 export const drawInteractables = () => {
-    for (let obj of interactables) {
-        gl.stencilMask(0x00);
-        gl.stencilFunc(gl.ALWAYS, 0, 0xff);
+  for (let obj of interactables) {
+    gl.stencilMask(0x00);
+    gl.stencilFunc(gl.ALWAYS, 0, 0xff);
 
-        // drawHighlight(obj.id, [1, 0, 0, 1]);
-        sprite.drawSprite(gl, obj._sprite, obj.pos, true)
-    }
+    // drawHighlight(obj.id, [1, 0, 0, 1]);
+    sprite.drawSprite(gl, obj._sprite, obj.pos, true)
+  }
 }
 
 /**
@@ -52,22 +52,22 @@ export const drawInteractables = () => {
  * @param {[number,number,number,number]} tint 
  */
 export const drawHighlight = (interactableID, tint) => {
-    const obj = interactables[interactableID]
-    if (!obj) return;
-    // gl.enable(gl.DEPTH_TEST);
-    gl.disable(gl.DEPTH_TEST);
-    gl.depthMask(false);
-    sprite.drawTex(gl, obj._sprite, {
-        x: obj.pos.x,
-        y: obj.pos.y,
-        z: -obj.pos.y + 0.01,
-    },
-        camera.pos,
-        [1.1, 1.1],
-        highlightShader,
-        {
-            u_tint: tint,
-            u_cutThreshold: 0,
-        });
-    gl.depthMask(true);
+  const obj = interactables[interactableID]
+  if (!obj) return;
+  // gl.enable(gl.DEPTH_TEST);
+  gl.disable(gl.DEPTH_TEST);
+  gl.depthMask(false);
+  sprite.drawTex(gl, obj._sprite, {
+    x: obj.pos.x,
+    y: obj.pos.y,
+    z: -obj.pos.y + 0.01,
+  },
+    camera.pos,
+    [1.1, 1.1],
+    highlightShader,
+    {
+      u_tint: tint,
+      u_cutThreshold: 0,
+    });
+  gl.depthMask(true);
 }
